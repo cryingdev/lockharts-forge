5137c9f78f578600485b7403b3f344d6b968de44
cryingdev <33308581+cryingdev@users.noreply.github.com>
2026-01-10 20:48:24 +0900

    refactor: Comment out unused Phaser scene properties
    These properties are inherited from Phaser.Scene but are not directly used within the WorkbenchScene class. Commenting them out reduces noise and clarifies the code's dependencies.

--- game/WorkbenchScene.ts
+++ game/WorkbenchScene.ts
@@ -58,15 +58,15 @@ function rdp(points: Point[], epsilon: number): Point[] {
 }
 
 export default class WorkbenchScene extends Phaser.Scene {
-  public add!: Phaser.GameObjects.GameObjectFactory;
-  public tweens!: Phaser.Tweens.TweenManager;
-  public scale!: Phaser.Scale.ScaleManager;
-  public input!: Phaser.Input.InputPlugin;
-  public time!: Phaser.Time.Clock;
-  public load!: Phaser.Loader.LoaderPlugin;
-  public textures!: Phaser.Textures.TextureManager;
-  public events!: Phaser.Events.EventEmitter;
-  public cameras!: Phaser.Cameras.Scene2D.CameraManager;
+  // public add!: Phaser.GameObjects.GameObjectFactory;
+  // public tweens!: Phaser.Tweens.TweenManager;
+  // public scale!: Phaser.Scale.ScaleManager;
+  // public input!: Phaser.Input.InputPlugin;
+  // public time!: Phaser.Time.Clock;
+  // public load!: Phaser.Loader.LoaderPlugin;
+  // public textures!: Phaser.Textures.TextureManager;
+  // public events!: Phaser.Events.EventEmitter;
+  // public cameras!: Phaser.Cameras.Scene2D.CameraManager;
 
   private interactables: Interactable[] = [];
   
